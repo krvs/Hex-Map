4f23645526b7adaa7db568f42313989760e5e75e
Kirill V <kvasilev@croc.ru>
2019-07-24 10:45:44 +0300

    Добавлена сетка для карты

---
+++ Hex Map/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    [SerializeField] private int _width = 6;
+    [SerializeField] private int _height = 6;
+    [SerializeField] private HexCell _cellPrefab;
+    [SerializeField] private TextMeshProUGUI _cellLabelPrefab;
+
+    private HexCell[] _cells;
+    private Canvas _gridCanvas;
+    private HexMesh _hexMesh;
+
+    private void Awake()
+    {
+        _gridCanvas = GetComponentInChildren<Canvas>();
+        _hexMesh = GetComponentInChildren<HexMesh>();
+
+        _cells = new HexCell[_height * _width];
+
+        for (int z = 0, i = 0; z < _height; z++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        _hexMesh.Triangulate(_cells);
+    }
+
+    private void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * .5f - z / 2f) * (HexMetrics.InnerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.OuterRadius * 1.5f);
+
+        var cell = _cells[i] = Instantiate<HexCell>(_cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.position = position;
+
+        var label = Instantiate<TextMeshProUGUI>(_cellLabelPrefab);
+        label.rectTransform.SetParent(_gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x + "\n" + z;
+    }
+}
---
+++ Hex Map/Assets/Scripts/HexMesh.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer)) ]
+public class HexMesh : MonoBehaviour
+{
+    private Mesh _hexMesh;
+    private List<Vector3> _vertices;
+    private List<int> _triangles;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = _hexMesh = new Mesh();
+        _hexMesh.name = "Hex Mesh";
+        _vertices = new List<Vector3>();
+        _triangles = new List<int>();
+    }
+
+    public void Triangulate(HexCell[] cells)
+    {
+        _hexMesh.Clear();
+        _vertices.Clear();
+        _triangles.Clear();
+
+        foreach (var cell in cells)
+        {
+            Triangulate(cell);
+        }
+
+        _hexMesh.vertices = _vertices.ToArray();
+        _hexMesh.triangles = _triangles.ToArray();
+        _hexMesh.RecalculateNormals();
+    }
+
+    private void Triangulate(HexCell cell)
+    {
+        var center = cell.transform.localPosition;
+        AddTriangle(center, center + HexMetrics.Corners[0], center + HexMetrics.Corners[1]);
+    }
+
+    private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        var vertexIndex = _vertices.Count;
+        _vertices.Add(v1);
+        _vertices.Add(v2);
+        _vertices.Add(v3);
+        _triangles.Add(vertexIndex);
+        _triangles.Add(vertexIndex + 1);
+        _triangles.Add(vertexIndex + 2);
+    }
+}
